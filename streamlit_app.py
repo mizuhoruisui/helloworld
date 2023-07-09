c44c597a3cc4de0809af0c135febeb918daec405
Mizuho Yashiro <m.yashiro@truestar.co.jp>
2023-07-10 08:35:19 +0900

    add new file

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,11 +1,4 @@
 import streamlit as st
-import pandas as pd
-import numpy as np
-
-st.header('Line chart')
-
-chart_data = pd.DataFrame(
-     np.random.randn(20, 3),
-     columns=['a', 'b', 'c'])
-
-st.line_chart(chart_data)
+st.header('st_selectbox')
+option = st.selectbox('what is your favorite color?','Blue','Red','Green')
+st.write('Your favorite color is',option)
