726ce32edeb00b018a91b6e2bee156ebea8e5150
Mizuho Yashiro <m.yashiro@truestar.co.jp>
2023-07-11 08:07:09 +0900

    add new file

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,7 +4,7 @@ st.header('st.multiselect')
 
 options = st.multiselect(
     'What ar your favolite colors',
-    ['Green','Yellow','Red','Blue',
-     ['Yellow','Red']])
+    ['Green','Yellow','Red','Blue'],
+     ['Yellow','Red'])
 
 st.write('You selected:',options)
