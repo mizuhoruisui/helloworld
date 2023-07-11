e34029a1779bec48c80a9676d3e9349a24d947d0
Mizuho Yashiro <m.yashiro@truestar.co.jp>
2023-07-12 08:38:55 +0900

    add new file

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,39 +1,18 @@
 import streamlit as st
-from datetime import time, datetime
 
-st.header('st.slider')
+st.header('st.checkbox')
 
-#例1
+st.write('What would you like to order?')
 
-st.subheader('Slider')
+icecream = st.checkbox('Ice cream')
+coffee = st.checkbox('Coffee')
+cola = st.checkbox('Cola')
 
-age = st.slider('How old are you?', 0, 130, 25)
-st.write("I'm ", age, 'years old')
+if icecream:
+    st.write("Great!Here's some more ??")
 
-#例2
+if coffee:
+    st.write("Okay,here's some coffee ?")
 
-st.subheader('Range slider')
-
-values = st.slider(
-     'Select a range of values',
-     0.0, 100.0, (25.0, 75.0))
-st.write('Values:', values)
-
-#例3
-
-st.subheader('Range time slider')
-
-appointment = st.slider(
-     "Schedule your appointment:",
-     value=(time(11, 30), time(12, 45)))
-st.write("You're scheduled for:", appointment)
-
-#例4
-
-st.subheader('Datetime slider')
-
-start_time = st.slider(
-     "When do you start?",
-     value=datetime(2020, 1, 1, 9, 30),
-     format="MM/DD/YY - hh:mm")
-st.write("Start time:", start_time)
+if cola:
+    st.write("Here's you go ??")
