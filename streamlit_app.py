79b12b2d572dc4877fea91a5647829c5ced983a9
Mizuho Yashiro <m.yashiro@truestar.co.jp>
2023-07-11 16:54:55 +0900

    add new file

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,11 +1,39 @@
 import streamlit as st
-import pandas as pd
-import numpy as np
+from datetime import time, datetime
 
-st.header('Line chart')
+st.header('st.slider')
 
-chart_data = pd.DataFrame(
-     np.random.randn(20, 3),
-     columns=['a', 'b', 'c'])
+#例1
 
-st.line_chart(chart_data)
+st.subheader('Slider')
+
+age = st.slider('How old are you?', 0, 130, 25)
+st.write("I'm ", age, 'years old')
+
+#例2
+
+st.subheader('Range slider')
+
+values = st.slider(
+     'Select a range of values',
+     0.0, 100.0, (25.0, 75.0))
+st.write('Values:', values)
+
+#例3
+
+st.subheader('Range time slider')
+
+appointment = st.slider(
+     "Schedule your appointment:",
+     value=(time(11, 30), time(12, 45)))
+st.write("You're scheduled for:", appointment)
+
+#例4
+
+st.subheader('Datetime slider')
+
+start_time = st.slider(
+     "When do you start?",
+     value=datetime(2020, 1, 1, 9, 30),
+     format="MM/DD/YY - hh:mm")
+st.write("Start time:", start_time)
