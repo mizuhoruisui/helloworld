d49f04f43f45f56dc821008e72892fa391e2d127
Mizuho Yashiro <m.yashiro@truestar.co.jp>
2023-07-13 19:26:02 +0900

    add new file

--- streamlit_app.py
+++ streamlit_app.py
@@ -8,4 +8,3 @@ st.header('`streamlit_pandas_profiling`')
 df = pd.read_csv('https://raw.githubusercontent.com/dataprofessor/data/master/penguins_cleaned.csv')
 
 pr = df.profile_report()
-st_profile_report(pr)
